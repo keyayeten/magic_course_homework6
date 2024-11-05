fee283db342a5276d54fcbfc1534c8335cc940db
Daria Milyakova <103563978+GithubAgitator@users.noreply.github.com>
2024-11-05 21:48:49 +0300

    task5

--- task5.py
+++ task5.py
@@ -6,3 +6,21 @@
 
 # P.s. фуннкции можно не только передавать как аргумент,
 # но и складывать в список
+
+dicts = [{'name': 'Leon',  'age': 22}, {'name': 'Olga', 'age': 20}, {'name': 'Peter', 'age': 50}, {'name': 'Pavel', 'age': 55}]
+def multi_filter(func: callable, dicts):
+    lst = []
+    for i in dicts:
+        if func(i):
+            lst.append(i)
+    print(lst)
+
+def filtres(x):
+    filter_1 = x['age'] > 25
+    filter_2 = len(x['name']) > 4
+    n = x['name']
+    filter_3 = n.startswith('P')
+    return filter_1 and filter_2 and filter_3
+
+
+multi_filter(filtres, dicts)
