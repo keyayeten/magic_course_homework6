6149acce9b561621e4cd43ac8fde56255758d904
Zhukovton <zhukovton@gmail.com>
2024-11-05 17:24:26 +0300

    Добавил 6 домашку

--- task2.py
+++ task2.py
@@ -5,6 +5,28 @@
 # Функция должна применить operation ко всем
 # элементам списка и вернуть результат.
 
+def aggregate(func: callable, list1: list) -> int:
+    count = list1[0]
+    for i in list1[1:]:
+        count = func(count, i)
+    return count
+
+
+def operation_sum(x, y):
+    x += y
+    return x
+
+
+def operation_ymnozh(x, y):
+    x *= y
+    return x
+
+
+_lst = [1, 2, 3, 4, 5]
+
+print(aggregate(operation_sum, _lst))
+print(aggregate(operation_ymnozh, _lst))
+
 # Пример:
 # [1, 2, 3, 4, 5]
 # Передаваемая функция: функция сложения
